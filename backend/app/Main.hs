d6646cfba00b0b96b8feb968b41475b58a0ecbb2
mhcurylo <mhcurylo@gmail.com>
2017-04-09 08:29:40 +0100

    Websockets work started

--- backend/app/Main.hs
+++ backend/app/Main.hs
@@ -1,6 +1,25 @@
+{-# LANGUAGE OverloadedStrings   #-}
+
 module Main where
 
 import Lib
+import qualified Data.Text                      as T
+import qualified Network.HTTP.Types             as Http
+import qualified Network.Wai                    as Wai
+import qualified Network.Wai.Handler.Warp       as Warp
+import qualified Network.Wai.Handler.WebSockets as WS
+import qualified Network.WebSockets             as WS
 
 main :: IO ()
-main = someFunc
+main = Warp.run 3000 app
+
+app :: Wai.Application
+app = WS.websocketsOr WS.defaultConnectionOptions wsApp backupApp
+    where
+      wsApp :: WS.ServerApp
+      wsApp pending_conn = do
+        conn <- WS.acceptRequest pending_conn
+        print "Connected"
+
+backupApp :: Wai.Application
+backupApp _ respond = respond $ Wai.responseLBS Http.status400 [] "Not a WebSocket request"
--- plugin/src/background.ts
+++ plugin/src/background.ts
@@ -1,9 +1,11 @@
 import { next } from './app/app';
 import { fmap, Maybe } from './libs/maybe';
 import { addPageEvent } from './reducers/pages/actions/action.creators';
-import { Page, PageEvent, Place} from './state/state.interface';
+import { Page, PageEvent, Place } from './state/state.interface';
 
 (window as any).next = next;
+
+// MOCK FOR NOW
 
 const arriveAtNewPlace = fmap((url: string, title: string): void => {
     const place: Place = {
@@ -22,7 +24,19 @@ const arriveAtNewPlace = fmap((url: string, title: string): void => {
     next((addPageEvent(pageEvent)));
 });
 
-const tabActivated = (activeInfo: chrome.tabs.TabActiveInfo): void =>
-    chrome.tabs.get(activeInfo.tabId, (tab) => arriveAtNewPlace(tab.url, tab.title));
+// SERVER COMMUNICATION
+
+const ws = new WebSocket('ws://localhost:3000/');
+
+ws.onmessage = console.log.bind(console);
+
+// ASSIGNING LISTENERS
+
+const tabActivated = (activeInfo: chrome.tabs.TabActiveInfo): void => {
+    chrome.tabs.get(activeInfo.tabId, ({ url, title }: chrome.tabs.Tab) => {
+        arriveAtNewPlace(url, title);
+        ws.send(JSON.stringify({ url, title }))
+    });
+};
 
 chrome.tabs.onActivated.addListener(tabActivated);
